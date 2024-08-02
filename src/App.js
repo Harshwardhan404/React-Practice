489437da09a7441fb058074640025ec907a2c58f
Harshwardhan Patil <patil.04042003@gmail.com>
2024-08-02 22:20:13 +0530

    files initialized

--- src/App.js
+++ src/App.js
@@ -1,57 +1,53 @@
-// import './App.module.css';
-// import Profile from './components/Navbar';
-
- import styles from './App.module.css'
+ import { useState } from 'react';
+// import styles from './App.module.css'
 
 
 function App() {
 
+
+//   const [text,setText]= useState('')
+
+
+// const handleOnChange = (event)=>{
+//   setText(event.target.value)
+// }
+
+
+// const [showText, setShowText] = useState(true)
+
+// const handleOnClick = ()=>{
+// setShowText(!showText)
+// }
+
+const [color, setColor]= useState("green")
+
+const ColorChanger = ()=>{
+  
+  setColor(color === 'green'? 'red': 'green')
+
+}
+
   return (
-   <>
-   
- <User name = "Harsh" age={23} email = "pati.04@gmail.com"/>
- <User name = "Raj" age={12} email = "raj.04@gmail.com"/>
- <User name = "Shivam" age={25} email = "shivam.04@gmail.com"/> 
-
- <Company salary ={320000} position= "software Developer" company = "Google"/>
- <Company salary ={220000} position= "Tester" company = "Amazon"/>
- <Company salary ={310000} position= "DotNET Developer" company = "Meta Inc"/>
+   <div>
+  
+  {/* <input type='text' onChange={handleOnChange}></input>
+  {text} */}
 
 
-  <div className={styles.App}>Hello Pedro</div>
-  <div className={styles.Hello}>Hello Pedro</div>
 
-   </>
+      {/* {showText && <h1>I am Harsh</h1> }      */}
+
+  {/* <button onClick={handleOnClick}>Show/Hide</button> */}
+
+
+
+      <h1 style={ { color : color}}>My name is Harsh</h1>
+      <button onClick={ColorChanger}>Change Color</button>
+
+
+
+   </div>
   )
 }
-
-
-const User = (props)=>{
-return (
-<>
-
-<h2>{props.name}</h2>
-<h2>{props.age}</h2>
-<h2>{props.email}</h2>
-
-
-</>
-)
-}
-
-
-const Company = (props)=>{
-  return (
-  <>
-  <h1>{props.salary}</h1>
-  <h1>{props.position}</h1>
-  <h1>{props.company}</h1>
-
-
-
-  </>
-  )
-}
-
 
 export default App;
