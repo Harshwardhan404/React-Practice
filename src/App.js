d96baabb108a4777a30254085e8d0d0a70bdaf93
Harshwardhan Patil <patil.04042003@gmail.com>
2024-08-04 18:02:12 +0530

    main branch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.module.css';
-// import TheUseEffect from './TheUseEffect';
-import Effecttwo from './Effecttwo';
 
 function App() {
 
   return (
    <>
 
-   <Effecttwo/>
- {/* <TheUseEffect/> */}
+  
 
    </>
   )
