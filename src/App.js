3aa3b4ae7ac164926985fa25e4fdbac47c402bc3
Harshwardhan Patil <patil.04042003@gmail.com>
2024-08-15 09:18:06 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useState } from 'react';
 import './App.module.css';
 
 function App() {
 
+
+  const [excuse, setExcuse] = useState(null)
+
+
+
+
+
+const handleExcuse = (excuse) =>{
+
+  fetch(`https://excuser-three.vercel.app/v1/excuse/${excuse}`).then((response)=>{
+    return response.json()
+  }).then((data)=> setExcuse(data[0].excuse))
+
+}
+
+  
   return (
    <>
 
+    <button onClick={()=>{handleExcuse('family')}}>Family</button>
+    <button onClick={()=>{handleExcuse('office')}}>Office</button>
+    <button onClick={()=>{handleExcuse('children')}} >Children</button>
   
+
+    <h1>{excuse}</h1>
 
    </>
   )
