5d70b8482e21fd2ca43a849d4d07b4e2fcacf5ea
Harshwardhan Patil <patil.04042003@gmail.com>
2024-08-15 08:50:00 +0530

    predict age example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import './App.module.css';
 
 function App() {
 
+  const [ name, setName] = useState("")
+  const [ApiObject, setApiObject] = useState(null)
+
+  const handleOnChange = (event)=>{
+    setName(event.target.value)
+  }
+
+  const handleAge = ()=>{
+   fetch(`https://api.agify.io/?name=${name}`).then(res=> res.json()).then((age)=> setApiObject(age))
+  }
+
   return (
    <>
 
+  <input onChange={handleOnChange}/>
+  <button onClick={handleAge}>Predict Age</button>
   
+  <p>Predicted age is : {ApiObject?.age}</p>
+  <p>Name is : {ApiObject?.name}</p>
+  <p>Number of Observations: {ApiObject?.count}</p>
 
    </>
   )
