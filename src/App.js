cd9e8c5ab422a258b3207139fae852c219b26458
Harshwardhan Patil <patil.04042003@gmail.com>
2024-09-08 09:42:08 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.module.css';
+import Home from './components/Home';
+import Addmore from './components/Addmore';
+import Menu from './components/Menu';
 
 function App() {
 
   return (
    <>
 
+<nav>
+
+  <ul>
+      <li> <Link to="/">Home</Link></li>
+      <li> <Link to="/more"> Add More</Link> </li>
+      <li>  <Link to="/menu">Menu</Link>  </li>
+  </ul>
+
+</nav>
+
+
+
+<Routes>
+
+    <Route path='/' element = {<Home/>}/>
+    <Route path='/more' element = {<Addmore/>}/>
+    <Route path='/menu' element = {<Menu/>}/>
+  
+</Routes>
   
 
    </>
---
+++ src/components/Addmore.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Addmore() {
+  return (
+    <div>
+      Add More
+    </div>
+  )
+}
